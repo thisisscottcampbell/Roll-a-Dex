d57fe30b4eb4f39bb97fa289f3938d7041f1c3d3
Scott Campbell <stc9486@gmail.com>
2020-12-09 16:02:32 -0500

    update tech

--- cra-rolla/routes/list.js
+++ cra-rolla/routes/list.js
@@ -4,6 +4,7 @@ const verifyUser = require('../controllers/auth')
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const Tech = require('../models/Tech');
+const { networkInterfaces } = require('os');
 
 
 router.get('/', verifyUser, 
@@ -48,8 +49,33 @@ router.post('/', verifyUser,
     }
 })
 
-router.put('/:id', (req, res) => {
-  res.send('Update tech')
+router.put('/:id', verifyUser, 
+  async (req, res) => {
+
+  const { title, note } = req.body;
+
+  const techVals = {};
+  
+  if (title) techVals.title = title;
+  if (note) techVals.note = note;
+
+  try {
+    let editTech = await Tech.findById(req.params.id);
+
+    if (!editTech) return res.status(404).json({ msg: 'Cannot locate item' });
+
+    if (editTech.user.toString() !== req.user.id) return res.status(401).json({ msg: "No PUT auth"});
+    
+    editTech = await Tech.findByIdAndUpdate(req.params.id,
+      { $set: techVals },
+      { new: true });
+
+      res.json(editTech);
+  }
+  catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
 })
 
 router.delete('/:id', (req, res) => {
